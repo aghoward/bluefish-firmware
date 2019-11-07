d2087bef769a1dc8935402a55bffda40b49a0d97
Austin Howard <austinghoward@gmail.com>
2019-11-07 08:28:14 -0500

    Give project a proper name
    Project has been renamed, for too long this was simply called
    "eeprom-filesystem" it is much more than that now. It has been renamed
    "bluefish" with this hardware based component getting the name
    "bluefish-firmware". Bluefish is inspired by the name of the encryption
    algorithm to be used on the client side, "Twofish", and a play on Dr.
    Seuss' story "One fish two fish red fish blue fish".

--- bluefish-firmware.ino
+++ bluefish-firmware.ino
@@ -7,26 +7,27 @@
 #include "char_string.h"
 
 #include "api.h"
+#include "binary_api.h"
 #include "text_api.h"
 
 std::unique_ptr<API> _api;
 
 void setup()
 {
+    pinMode(1, OUTPUT);
+    pinMode(2, OUTPUT);
     Wire.begin();
 
+    digitalWrite(1, HIGH);
     Serial.begin(115200);
     while (!Serial) {}
-    Serial.println("Begin...");
 
-    _api = std::make_unique<TextAPI>();
-    Serial.println("Filesystem initialized!");
+    _api = std::make_unique<BinaryAPI>();
+    digitalWrite(1, LOW);
 }
 
 void loop()
 {
-    while (!Serial.available()) {}
-
     auto command = _api->read_command();
     _api->process_command(command);
 }
